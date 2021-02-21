1b04a9874195bc7d9604753774ff994b672dc8f3
korotkovandrew <andrej_korotkov@mail.ru>
2021-02-21 21:53:40 +0300

    Basic movement added

--- Le Pool/ball.h
+++ Le Pool/ball.h
@@ -1,12 +1,30 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-class ball : sf::Sprite
-{
-	float x, y;
-	
-	ball(int _x = 0, int _y = 0) : x(_x), y(_y) {
-		
-	};
-};
+//class Ball {
+//private:
+//	float x, y, w, h; // coordinates, width, height
+//	float dx, dy, speed; // x and y axises acceleration, ball's speed
+//	float dir;
+//	Image image;
+//	Texture texture;
+//	Sprite sprite;
+//public:
+//	Ball(String F, int X, int Y, float W, float H) {
+//		w = W, h = H;
+//		x = X, y = Y;
+//		image.loadFromFile("images//" + F);
+//		texture.loadFromImage(image);
+//		sprite.setTexture(texture);
+//		sprite.setTextureRect(IntRect(w, h, w, h));
+//	}
+//
+//};
+
+namespace Ball {
+	const float R = 10.f;
+	const float D = 2*R;
+	const float XSPEED = 1.f;
+	const float YSPEED = 1.f;
+}
 
--- Le Pool/main.cpp
+++ Le Pool/main.cpp
@@ -1,33 +1,81 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include "ball.h"
 
-const int SC_W = 320;
-const int SC_H = 280;
+const unsigned int SC_W = 720;
+const unsigned int SC_H = 360;
+
+
 
 
 using namespace sf;
 int main()
 {
-	RenderWindow window(VideoMode(SC_W, SC_H), "SFML Works!");
+	ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	RenderWindow window(VideoMode(SC_W, SC_H), "SFML Works!", Style::Default, settings);
 
-	bool gameBegin = true;
-	// Main Loop
+	Clock clock;
+
+	bool bounce = false;
+	int dx = 1, dy = 1;
+	float x, y;
+	float a = 0;
+
+	CircleShape ball(Ball::R);
+	ball.setFillColor(Color::Cyan);
+	ball.setPosition(SC_W / 2 - Ball::R, SC_H / 2 - Ball::R);
+
+	////---- Main Loop ----////
 	while (window.isOpen())
 	{
+		float time = clock.getElapsedTime().asMicroseconds();
+		clock.restart();
+		time = time / 2000;
+
 		Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
 				window.close();
 		}
+
+		// определение координат шара
+		x = ball.getPosition().x, y = ball.getPosition().y;
+
+		// передвижение
+		if (!bounce)
+		{
+			if (Keyboard::isKeyPressed(Keyboard::Up) || Keyboard::isKeyPressed(Keyboard::W))
+				ball.move(0, -1 * time * Ball::YSPEED);
+			if (Keyboard::isKeyPressed(Keyboard::Down) || Keyboard::isKeyPressed(Keyboard::S))
+				ball.move(0, 1 * time * Ball::YSPEED);
+			if (Keyboard::isKeyPressed(Keyboard::Left) || Keyboard::isKeyPressed(Keyboard::A))
+				ball.move(-1 * time * Ball::XSPEED, 0);
+			if (Keyboard::isKeyPressed(Keyboard::Right) || Keyboard::isKeyPressed(Keyboard::D))
+				ball.move(1 * time * Ball::XSPEED, 0);
+
+			// checking OoB
+			if (x < 0 || x + Ball::D > SC_W || y < 0 || y + Ball::D > SC_H) { bounce = true; a = 4; };
+		}
+		else
+		{
+			a -= 0.001;
+			if (a < 0 || Mouse::isButtonPressed(Mouse::Right)) { bounce = false; a = 0; }
+
+			if (y < 0) dy = -dy;
+			else if (x < 0) dx = -dx;
+			else if (y + Ball::D > SC_H) dy = -dy;
+			else if (x + Ball::D > SC_W) dx = -dx;
+
+			ball.move(dx * time * a * Ball::XSPEED, dy * time * a * Ball::YSPEED);
+		}
+
+
+		ball.setFillColor(bounce ? Color::Green : Color::Red);
+		window.clear(Color::White);
+		window.draw(ball);
 		window.display();
 	}
-	window.clear(Color::White);
-
-	
-
-	// logic
-
-
 	return 0;
 }
